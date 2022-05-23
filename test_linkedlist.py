b585a42be957e42e38d2a1ed2513d2e3041b13a6
Nauman Arif <95040793+naumanaarif@users.noreply.github.com>
2022-05-24 00:06:46 +0500

    Added test file for linkedlist.py

---
+++ test_linkedlist.py
@@ -0,0 +1,19 @@
+import unittest
+
+from linkedlist import Node, LinkedList
+
+
+class Test_LinkedList(unittest.TestCase):
+
+    def test_insert_byNonIterableValues(self):
+        ...
+
+    def test_insert_byIterableValues(self):
+        ...
+
+    def test_insert_byIndex(self):
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
