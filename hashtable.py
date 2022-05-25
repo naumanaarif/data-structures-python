197e809434009807b5357a80168a96705c407bc8
Nauman Arif <95040793+naumanaarif@users.noreply.github.com>
2022-05-25 21:45:00 +0500

    Added hash() method

--- hashtable.py
+++ hashtable.py
@@ -6,9 +6,31 @@ class HashTable():
 
     def __init__(self, max=100) -> None:
         self.MAX = max
-        self.arr = [LinkedList() for _ in range(self.MAX)]
+        self.llist = [LinkedList() for _ in range(self.MAX)]
+
+    def hash(self, key: str):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+    def __getitem__(self, key: int | str):
+        ...
+
+    def __setitem__(self, key: int | str, value):
+        ...
+
 
 if __name__ == '__main__':
     table = HashTable(10)
-    for llist in table.arr:
-        print(llist)
+
+    table.add("AbuBakr", 123)
+    table.add("Umar", 124)
+    table.add("Umar", 127)
+    table.add("Uthman", 125)
+    table.add("Ali", 126)
+
+    for row in table.llist:
+        print(row)
+
+    print(table)
